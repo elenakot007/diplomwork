0f4ca45a49334120df45378ff1ec6127bae88487
elenakot007 <alexandrakot007@gmail.com>
2020-05-15 16:44:10 +0300

    changes at 15.05.2020

--- js/script.js
+++ js/script.js
@@ -20,24 +20,24 @@ $(document).ready(function(){
 
 	let owl = $('.owl-carousel');
 		
-	$(".arrow-end").click(function(){
+	$(".examples__arrow-end").click(function(){
 		owl.trigger("next.owl.carousel");
 	});
 		
-	$(".arrow-start").click(function(){
+	$(".examples__arrow-start").click(function(){
 		owl.trigger("prev.owl.carousel");
 	});
 
 	$(".elements").removeClass("anim");
 
-	var menuNav = $(".navigation");
+	var menuNav = $(".top-contacts__navigation");
 
-	$(".menu-mobile").click(function() {
+	$(".top-contacts__menu-mobile").click(function() {
 		menuNav.toggleClass("navigation-mobile");	
   })
 
 //Показать модальное окно для заказа звонка по телефону
-  $(".button-first").click(function(){
+  $(".button-call").click(function(){
 	$(".showform-phone").fadeIn();
   })
 
@@ -47,26 +47,26 @@ $(document).ready(function(){
   })
 
 //Показать модальное окно для отправки сообщения с e-mail
-  $(".button-second").click(function(){
+  $(".button-message").click(function(){
 	$(".showform-mail").fadeIn();
   })
 
 
 //Закрыть модальное окно
-  $(".message-close").click(function(){
-	$(".name").val("");
-	$(".phone-text").val("");
-	$(".e-mail").val("");
-	$(".message-text").val("");
-	$(".message-background").fadeOut();
+  $(".popup-message__close-button").click(function(){
+	$(".popup-message__name-input").val("");
+	$(".popup-message__phone-input").val("");
+	$(".popup-message__mail-input").val("");
+	$(".popup-message__text-input").val("");
+	$(".popup-message").fadeOut();
   })
 });
 
 //Закрыть модальное окно по клику вне окна
 $(document).mouseup(function (e) { 
-	var popup = $('.message-background');
+	var popup = $('.popup-message');
 	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
-		$('.message-background').fadeOut();
+		$('.popup-message').fadeOut();
 	}
 });
 
